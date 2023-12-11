147b49b9215fd17df87aa1a0402338348b57f3a4
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-12-11 13:13:21 +0200

    modified tasks.py

--- snakegame/tasks.py
+++ snakegame/tasks.py
@@ -8,12 +8,16 @@ def start(ctx):
 
 @task
 def test(ctx):
-    ctx.run("coverage run --branch -m pytest src")
+    ctx.run("pytest src")
 
 
 @task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+
+@task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage report")
     ctx.run("coverage html")
 
 
